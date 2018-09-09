656e8e2411d16802e0adff8a1202b705d684f334
CurbYourStrangeness <36433882+CurbYourStrangeness@users.noreply.github.com>
2018-09-08 19:10:52 -0500

    Added comments explaining functionality

--- garrysmod/lua/matproxy/player_weapon_color.lua
+++ garrysmod/lua/matproxy/player_weapon_color.lua
@@ -11,15 +11,20 @@ matproxy.Add( {
 	bind = function( self, mat, ent )
 
 		if ( !IsValid( ent ) ) then return end
-
+                
+		--Collects weapon owner value from ent and binds it to local variable
 		local owner = ent:GetOwner()
+			
+		--Checks if weapon's owner is a player or valid owner
 		if ( !IsValid( owner ) or !owner:IsPlayer() ) then return end
-
+		--gets the weapon color as a local varible
 		local col = owner:GetWeaponColor()
+		--Checks if the stored property is a vector
 		if ( !isvector( col ) ) then return end
-
+		
+		--Creates local variable used to modify color
 		local mul = ( 1 + math.sin( CurTime() * 5 ) ) * 0.5
-
+		--modifues color based on CurTime()
 		mat:SetVector( self.ResultTo, col + col * mul )
 
 	end
